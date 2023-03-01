b6b923b76df0ee2ca65cfacba19087124aa153de
Burkay <aburkaykinik@gmail.com>
2023-03-02 00:13:21 +0300

    First commit for server and client. Untested

--- client.py
+++ client.py
@@ -1,23 +1,15 @@
 import socket
+import sys
+
 
 port = 19143
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.settimeout(10)
 
-s.bind(('',port))
-s.listen()
+s.connect((sys.argv[1],port))
 
-
-conn,addr = s.accept()
-with conn:
-    conn.send("Connected to the device.".encode())
-    while(True):
-        command = conn.recv(1024).decode()
-        if(command == "exit"):
-            conn.close()
-            break
-        #if(command == )
-
-s.close()
-
+while(True):
+    command = input("&>   ")
+    
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+from libs import command_split
+from libs import execute_command
 
 port = 19143
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,5 +13,19 @@ s.listen()
 
 conn,addr = s.accept()
 with conn:
-    
+    conn.send("Connected to the device.".encode())
+    while(True):
+        command = conn.recv(1024).decode()
+        if(command == "exit"):
+            conn.close()
+            break
+        command_list = command_split(command)
+        
+        if(command_list[0] == "shell"):
+            stdout,stderr = execute_command(command_list[1:])
+            conn.send(stdout)
+            continue
+
+
+s.close()
 
